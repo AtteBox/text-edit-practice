ba8c3fc5b453ed9919411665ec3d2903306a2c17
Atte Virtanen <90787140+AtteBox@users.noreply.github.com>
2024-10-07 18:31:10 +0300

    fix linting pt.2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
 
-export const roboto_mono = Roboto_Mono({
+const roboto_mono = Roboto_Mono({
   subsets: ["latin"],
   display: "swap",
 });
