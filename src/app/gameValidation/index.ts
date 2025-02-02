93ad025dda7f893a63a0529330d78e36eb37c890
Atte Virtanen <aivirtan@gmail.com>
2025-02-02 23:27:18 +0200

    add gamevalidation function

---
+++ src/app/gameValidation/index.ts
@@ -0,0 +1,46 @@
+import { getGermCount } from "../engines/game";
+import {
+  getActualInitialCursorPos,
+  startContentToText,
+} from "../engines/level";
+import { levels } from "../levels";
+import { calcTextarea } from "../virtualTextarea";
+
+export type IKeyRecording = {
+  level: number;
+  pressedKeys: { keyCombination: string[]; timestamp: number }[];
+}[];
+
+export function validateKeyRecording(keyRecording: IKeyRecording): boolean {
+  let previousKeyDownTimestamp = 0;
+  for (let levelNo = 1; levelNo <= levels.length; levelNo++) {
+    const level = levels[levelNo - 1];
+    const levelRecording = keyRecording.find(
+      (recording) => recording.level === levelNo,
+    );
+    if (!levelRecording) {
+      return false;
+    }
+
+    let text = startContentToText(level.startContent);
+    let cursorPos: number = getActualInitialCursorPos(
+      level.cursorStartPos,
+      level.startContent,
+    );
+    for (const pressedKey of levelRecording.pressedKeys) {
+      if (pressedKey.timestamp < previousKeyDownTimestamp) {
+        return false;
+      }
+      previousKeyDownTimestamp = pressedKey.timestamp;
+
+      const result = calcTextarea(cursorPos, text, pressedKey.keyCombination);
+      cursorPos = result.cursorPos;
+      text = result.text;
+    }
+
+    if (getGermCount(text) > 0) {
+      return false;
+    }
+  }
+  return true;
+}
